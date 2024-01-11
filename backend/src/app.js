28ea88257deae26d0f57bce4da86f82c95e8dbf2
Franmeza <ing.meza.francisco@gmail.com>
2024-01-11 18:52:18 -0500

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,19 +4,7 @@ import noteRoutes from "./routes/notes.routes.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
-  );
-  next();
-});
+app.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.json());
